9649a0ad670817a801f28ae1d64e184f2623f692
Scott Anderson <ascent12@hotmail.com>
2014-09-10 17:06:57 +1200

    update

--- problem3/main.c
+++ problem3/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
 
-#define NUM_PRIMES 1000000
+#define NUM_PRIMES 10000
 
 uint64_t primes[NUM_PRIMES];
 
@@ -22,6 +23,10 @@ int isprime(int n)
 
 uint64_t primefact(uint64_t n, int i, uint64_t sum)
 {
+	printf("i=%d\tprimes[i]=%lld\tsum=%lld\n", i, primes[i], sum);
+
+	int orig = i;
+
 	if (sum == n)
 		return primes[i - 1];
 
@@ -30,23 +35,35 @@ uint64_t primefact(uint64_t n, int i, uint64_t sum)
 
 	uint64_t ret;
 
-	if(!(ret = primefact(n, i + 1, sum * primes[i])))
-		while (!ret && primes[i] < sqrt(n))
-			ret = primefact(n, ++i, sum);
+	if(!(ret = primefact(n, i + 1, sum * primes[i]))) {
+		printf("i=%d\tprimes[i]=%d\tdid not work\n", i, primes[i]);
+		while (!ret && primes[i++] <= (uint64_t)ceil(sqrt(n)))
+			ret = primefact(n, i, sum);
+	}
 
+	printf("\t\ti=%d\tret=%lld\n", orig, ret);
 	return ret;
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	if (argc != 2)
+		return 0;
+
+	int n = atoi(argv[1]);
+
 	int i = 1;
 	int j = 0;
 
+	printf("Calculating the first %d primes...\n", NUM_PRIMES);
 	while (j < NUM_PRIMES)
 		if (isprime(++i))
 			primes[j++] = i;
+	printf("Done\n\n");
 
-	printf("%d\n", primefact(600851475143, 0, 1));
+	printf("Calulating largest prime factor of %d...\n", n);
+	printf("%lld\n", primefact(n, 0, 1));
+	printf("Done\n");
 
 	return 0;
 }
